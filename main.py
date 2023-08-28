22598350796429ef5d53973b6144f3823a312b05
quang <nguyenthequang080203@gmail.com>
2023-08-28 10:15:59 +0700

    post request

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/get-user/<user_id>")
+@app.route("/get-user/<user_id>") #default is GET
 def getUser(user_id):
     userData = {
         "user_id": user_id,
@@ -14,6 +14,13 @@ def getUser(user_id):
     if extra:
         userData['extra'] = extra
     return jsonify(userData), 200
+
+@app.route("/create-user", methods=["POST"])
+def create_user():
+    data = request.get_json() #get json payload from client
+    #database manipulation
+    return jsonify(data), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
