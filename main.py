75e2d0cef035c08099ec5be67a00e2e2c523746a
quang <nguyenthequang080203@gmail.com>
2023-08-28 10:08:41 +0700

    get request

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Home"
+@app.route("/get-user/<user_id>")
+def getUser(user_id):
+    userData = {
+        "user_id": user_id,
+        "name": "John Doe",
+        "email": "johndoe@email.com"
+    }
+    #get query parameter /get-user/2123?extra="hello"
+    extra = request.args.get("extra")
+    if extra:
+        userData['extra'] = extra
+    return jsonify(userData), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
